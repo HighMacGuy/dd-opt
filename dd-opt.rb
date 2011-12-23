61ce1e07c335b539e048da1268eb257e7e6810f6
Sam Kuper <sam.kuper@uclmail.net>
2011-12-23 03:04:56 +0000

    Abstracted block device properties into a Class. Working quite nicely now on OS X 10.6.8 Snow Leopard.

--- dd-opt.rb
+++ dd-opt.rb
@@ -1,3 +1,60 @@
+require 'os'
+
+class BlockDevice
+  attr_reader :path
+  attr_reader :mounted
+  attr_reader :total_bytes
+  attr_reader :free_bytes
+
+  def initialize(path)
+    if !FileTest.blockdev?(path)
+      abort(path + " is not a block device.")
+    else
+      @path = path
+
+      ## Initialise other BlockDevice attributes.
+      if OS::mac?
+
+        ## Get BlockDevice info string
+        @info = `diskutil info #{@path}`
+
+        ## Initialise @mounted attribute
+        if    @info.scan(/Mounted:\s*(\w*)/)[0][0] == "Yes" then @mounted = true
+        elsif @info.scan(/Mounted:\s*(\w*)/)[0][0] == "No"  then @mounted = false
+        else  abort("Could not determine whether " + @path + " is mounted.")
+        end
+
+        ## Initialise @total_bytes attribute
+        @total_bytes = @info.scan(/Total Size.*\((\d*) Bytes\)/)[0][0].to_i
+
+        ## Initialise @free_bytes attribute
+        # Mac OS X diskutil will not report the free space on an unmounted
+        # block device so we must mount this BlockDevice temporarily to find its
+        # free space and then immediately unmount it again to restore its state
+        # unless it was already mounted.
+        if !@mounted
+          if !system("diskutil mount " + @path)
+            abort ("Could not mount " + @path)
+          end
+          @free_bytes = @info.scan(/Volume Free Space.*\((\d*) Bytes\)/)[0][0].to_i
+          if !system("diskutil unmount " + @path)
+            abort ("Could not unmount " + @path)
+          end
+        else
+          @free_bytes = @info.scan(/Volume Free Space.*\((\d*) Bytes\)/)[0][0].to_i
+        end
+
+      else
+        abort("dd-opt does not yet work on operating systems besides Mac OS X. Help out by forking https://github.com/sampablokuper/dd-opt")
+      end
+    end
+  end
+end
+
+##
+# Parse arguments
+##
+
 if ARGV.length < 1
   abort("No dd infile specified.")
 elsif ARGV.length < 2
@@ -15,18 +72,12 @@ elsif ARGV.length == 4
   iterations = ARGV[3]
 end
  
-ddif = ARGV[0]
-ddof = ARGV[1]
+ddif = BlockDevice.new(ARGV[0])
+ddof = BlockDevice.new(ARGV[1])
 
-# Validate ddof, ddif
-
-if !FileTest.blockdev?(ddif)
-  abort(ddif + " is not a block device.")
-elsif !FileTest.blockdev?(ddof)
-  abort(ddof + " is not a block device.")
-end
-
+##
 # Validate ddbsmax
+##
 
 ddbsmax_array =  ddbsmax.scan(/(\D*)(\d*)(.*)/)[0]
 if ddbsmax_array[0] != ""
@@ -67,19 +118,13 @@ until ddbs > ddbsmax_bytes
 end
 effective_ddbsmax_bytes = ddbs / 2
 
-ddif_info = `diskutil info #{ddif}`
-ddif_bytes = ddif_info.scan(/Total Size.*\((\d*) Bytes\)/)[0][0].to_i
-if effective_ddbsmax_bytes > ddif_bytes
+if    effective_ddbsmax_bytes > ddif.total_bytes
   abort("bs cannot be larger than dd infile")
-end
-
-ddof_info = `diskutil info #{ddof}`
-ddof_bytes = ddof_info.scan(/Total Size.*\((\d*) Bytes\)/)[0][0].to_i
-if effective_ddbsmax_bytes > ddof_bytes
+elsif effective_ddbsmax_bytes > ddof.total_bytes
   abort("bs cannot be larger than dd outfile")
 end
 
-# Validate iterations
+# Validate "iterations" parameter
 
 iterations_array =  iterations.scan(/(\D*)(\d*)(\D*)/)[0]
 if iterations_array[0] != "" || iterations_array[2] != ""
@@ -88,39 +133,10 @@ else
   iterations = iterations.to_i
 end
 
-# Determine whether ddif and ddof are large enough that it's unnecessary
-# to clear the write and read caches for every single iteration/ddbs combination.
+# Check ddif used space is less than or equal to ddof total space
 
-ddif_bs = ddif_info.scan(/Total Size.*?(\d*)-Byte-Blocks\)/)[0][0].to_i
-ddof_bs = ddof_info.scan(/Total Size.*?(\d*)-Byte-Blocks\)/)[0][0].to_i
-dd_bs_max = [ddif_bs, ddof_bs].max
-
-ddbs = 1
-iseek = 0
-oseek = 0
-until ddbs == effective_ddbsmax_bytes 
-  count = effective_ddbsmax_bytes / ddbs
-    ##### Near-duplicate code lines below should be abstracted into a method. #####
-    if effective_ddbsmax_bytes % ddif_bs != 0
-      ddif_remainder = 1
-    else
-      ddif_remainder = 0
-    end
-    ddif_blocks_used = (effective_ddbsmax_bytes / ddif_bs) + ddif_remainder
-    iseek = iseek + ddif_blocks_used
-    if effective_ddbsmax_bytes % ddof_bs != 0
-      ddof_remainder = 1
-    else
-      ddof_remainder = 0
-    end
-    ddof_blocks_used = (effective_ddbsmax_bytes / ddof_bs) + ddof_remainder
-    oseek = oseek + ddof_blocks_used
-  ddbs = ddbs * 2
-end
-if (iseek * ddif_bs) > ddif_bytes || (oseek * ddof_bs) > ddof_bytes
-  fast_mode = false
-else
-  fast_mode = true
+if !((ddif.total_bytes - ddif.free_bytes) <= ddof.total_bytes)
+  abort(ddof.path + " is too small to accommodate the contents of " + ddif.path)
 end
 
 
@@ -128,61 +144,25 @@ end
 # Iterate
 #########
 
+puts "Starting tests..."
+
 z = 0
 results = Array.new()
 until z == iterations
   ddbs = 1
-  iseek = 0
-  oseek = 0
-  ddif_total_used = 0
-  ddof_total_used = 0
   until ddbs > ddbsmax_bytes
-    count = effective_ddbsmax_bytes / ddbs
-    if fast_mode == false || ddbs == 1
-      iseek = 0
-      oseek = 0
-      if !system("sync")
-        abort("Command 'sync' failed.")
-      elsif !system("purge")
-        abort("Command 'purge' failed.")
-      end
+    if !system("sync")
+      abort("Command 'sync' failed.")
+    elsif !system("purge")
+      abort("Command 'purge' failed.")
     end
-
-    stdout = `dd if=#{ddif} of=#{ddof} bs=#{ddbs} count=#{count} iseek=#{iseek} oseek=#{oseek} 2>&1`
-    # puts stdout
-
+    count = effective_ddbsmax_bytes / ddbs
+    stdout = `dd if=#{ddif.path} of=#{ddof.path} bs=#{ddbs} count=#{count} 2>&1`
     bytes_per_sec = stdout.scan(/\((\d*) bytes\/sec\)/)[0][0]
     padding = ddbsmax_bytes_length - ddbs.to_s.length + 2
     iteration_padding = iterations.to_s.length - z.to_s.length + 2
-    puts "Iteration: " + z.to_s + " " * iteration_padding + "bs: " + ddbs.to_s + " " * padding + "Bytes/sec: " + bytes_per_sec + " iseek: " + iseek.to_s + " oseek: " + oseek.to_s
+    puts "Iteration: " + z.to_s + " " * iteration_padding + "bs: " + ddbs.to_s + " " * padding + "Bytes/sec: " + bytes_per_sec
     results = results + [[ddbs, bytes_per_sec.to_i]]
-
-def get_seek_offset(bs_just_used, disk_bs, effective_ddbsmax_bytes)
-    if effective_ddbsmax_bytes % disk_bs != 0
-      remainder = 1
-    else
-      remainder = 0
-    end
-    next_bs_dd_blocks_used = (effective_ddbsmax_bytes / dd_bs) + remainder
-    iseek = iseek + ddif_blocks_used
-end 
-
-    ##### Near-duplicate code lines below should be abstracted into a method. #####
-    if effective_ddbsmax_bytes % ddif_bs != 0
-      ddif_remainder = 1
-    else
-      ddif_remainder = 0
-    end
-    ddif_blocks_used = (effective_ddbsmax_bytes / ddif_bs) + ddif_remainder
-    iseek = iseek + ddif_blocks_used
-    if effective_ddbsmax_bytes % ddof_bs != 0
-      ddof_remainder = 1
-    else
-      ddof_remainder = 0
-    end
-    ddof_blocks_used = (effective_ddbsmax_bytes / ddof_bs) + ddof_remainder
-    oseek = oseek + ddof_blocks_used
-
     ddbs = ddbs * 2
   end
   z = z + 1
